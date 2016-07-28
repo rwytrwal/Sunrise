f5031f8a62a8c79d20cccd2094591d0aff2e2ec3
rwytrwal <wytrwal123@gmail.com>
2016-07-28 12:55:20 +0200

    Poprawki

--- SunriseVector/Controllers/VectorController.cs
+++ SunriseVector/Controllers/VectorController.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 using Sunrise.Calculate;
-using SunriseVector.Models;
 
 
 namespace SunriseVector.Controllers
