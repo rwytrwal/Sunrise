60a6b6f74f418fcf2a2fedf36bfec9f168fd9885
rwytrwal <wytrwal123@gmail.com>
2016-07-28 15:09:56 +0200

    Update Vector.cs
    comment v1.1

--- Sunrise.Api/Models/Vector.cs
+++ Sunrise.Api/Models/Vector.cs
@@ -5,8 +5,14 @@ using System.Web;
 
 namespace SunriseVector.Models
 {
+    /// <summary>
+    /// SunriseVector class
+    /// </summary>
     public class Vector
     {
+        /// <summary>
+        /// SunriseVector X,Y,Z
+        /// </summary>
         public decimal VectorX { get; set; }
         public decimal VectorY { get; set; }
         public decimal VectorZ { get; set; }
